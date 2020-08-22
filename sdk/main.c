dd8aa5d0f8883c6d7f8840f1799f4bdd1bf7d65e
bin <tuduweb@qq.com>
2020-08-22 13:04:20 +0800

    子线程通过mailbox与主线程通信。

--- sdk/main.c
+++ sdk/main.c
@@ -2,23 +2,40 @@
 #include "stdint.h"
 #include "bin_mem.h"
 
-#define HEAP_SIZE  0x4000
-#define STACK_SIZE 0x400
+#define HEAP_SIZE  0x2000
+#define STACK_SIZE 0x200
 __attribute__((used)) __attribute__((section("HEAP"))) __attribute__((zero_init)) static char app_heap[HEAP_SIZE];
 __attribute__((used)) __attribute__((section("STACK"))) __attribute__((zero_init))  static char app_stack[STACK_SIZE];
 
-void lwt_thread_entry(void* parameter)
+rt_mailbox_t mb;
+
+int lwt_thread_entry(void* parameter)
 {
+	int cnt = 0;
     syscall(0x21,1234);
-	while(1);
+	while(1)
+	{
+		syscall(0x21,cnt++);
+		rt_thread_mdelay(2000);
+		if(cnt == 5)
+		{
+			rt_mb_send(mb, 666);
+		}else if(cnt > 5)
+		{
+			break;
+		}
+	}
+	return 0;
 }
 
-rt_thread_t thread;
+rt_thread_t thread = 0;
 
 
 
 
 int main(int argc, const char **argv, const char **envp){
+	
+	rt_ubase_t msg = 0;
 
 	app_heap_init(app_heap, app_heap + HEAP_SIZE);
 
@@ -33,24 +50,27 @@ int main(int argc, const char **argv, const char **envp){
 	//测试
 	syscall(0x21,0x99);
 
-	//uint32_t* pointer = (uint32_t*)0x70000004;
-	//*pointer = 1;
-	
-	//printf("test");
-	
+	mb = rt_mb_create("lwp", 1, RT_IPC_FLAG_FIFO);
+
 	thread = rt_thread_create("inlwp", lwt_thread_entry, 0, 0x400, 29, 200);
+	if(thread == RT_NULL)
+	{
+		syscall(0x21,0x00);
+		return 0;
+	}
 	rt_thread_startup(thread);
 
-	char* txt;
-	txt = malloc(100);
+	int cnt = 9999;
 
-	for(int i = 0;i < 100; ++i)
+	while(1)
 	{
-		*(txt + i) = '#';
+		if(rt_mb_recv(mb, &msg, RT_WAITING_FOREVER) == RT_EOK)
+		{
+			syscall(0x21,msg);
+			break;
+		}
+		syscall(0x21,cnt--);
 	}
-
-
-	while(1);
 	
 	return 0;//需要重定义sys_exit 否则会出现软件中断
 }
